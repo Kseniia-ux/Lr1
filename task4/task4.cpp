f3381d79c670b9db5cabf5c29996423f3a4a0641
Kseniia-ux <79336122+Kseniia-ux@users.noreply.github.com>
2021-02-22 15:46:17 +0300

    Rename task4.cpp to task4/task4.cpp

---
+++ task4/task4.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "math.h"
+double x = 8, f;
+using namespace std;
+void function()
+{
+	f=tan(2 * x) + 1 / cos(2 * x);
+}
+void main()
+{
+	
+	 function();
+	cout << "x = " << x << "; f = " << f << endl;
+	cin >> x;
+    function();
+	cout << "x = " << x << "; f = " << f << endl;
+}
