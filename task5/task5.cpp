7ae5deb0924c3906e813082239cf55c001b717bf
Kseniia-ux <79336122+Kseniia-ux@users.noreply.github.com>
2021-02-22 15:47:13 +0300

    Rename task5.cpp to task5/task5.cpp

---
+++ task5/task5.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "math.h"
+using namespace std;
+double function(double x);
+
+void main()
+{
+	double x = 8, f;
+	f = function(x);
+	cout << "x = " << x << "; f = " << f << endl;
+	cin >> x;
+	f = function(x);
+	cout << "x = " << x << "; f = " << f << endl;
+}
+
