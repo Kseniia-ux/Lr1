3efe6884ad87dc8b19f25cf9cae25c2773bcff27
Kseniia-ux <79336122+Kseniia-ux@users.noreply.github.com>
2021-02-22 15:39:27 +0300

    Rename task1.cpp to task1/task1.cpp

---
+++ task1/task1.cpp
@@ -0,0 +1,14 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+	double x = 8, f;
+	f = tan(2 * x) + 1 / cos(2 * x);
+	cout << "x = " << x << "; f = " << f << endl;
+	cin >> x;
+	f = tan(2 * x) + 1 / cos(2 * x);
+	cout << "x = " << x << "; f = " << f << endl;
+}
