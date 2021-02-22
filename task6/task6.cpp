d01e7a71adc3aef88ffe7e8382ebcb87992b2dc8
Kseniia-ux <79336122+Kseniia-ux@users.noreply.github.com>
2021-02-22 15:50:04 +0300

    Rename task6.cpp to task6/task6.cpp

---
+++ task6/task6.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "math.h"
+extern double x, f;
+using namespace std;
+void function();
+
+void main()
+{
+
+	function();
+	cout << "x = " << x << "; f = " << f << endl;
+	cin >> x;
+	function();
+	cout << "x = " << x << "; f = " << f << endl;
+}
